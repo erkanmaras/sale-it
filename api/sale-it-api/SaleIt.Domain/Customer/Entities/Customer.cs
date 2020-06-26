9be6c5d3a9aafd31da52bd0da3169f131b5ce75a
Goku <erkanmaras@gmail.com>
2020-06-26 15:59:42 +0300

    update customer

--- api/sale-it-api/SaleIt.Domain/Customer/Entities/Customer.cs
+++ api/sale-it-api/SaleIt.Domain/Customer/Entities/Customer.cs
@@ -9,9 +9,29 @@ namespace SaleIt.Domain.Customer.Entities
 {
     public class Customer
     {
-        public Guid CustomerId { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public CustomerAddress Address { get; set; }
+        private Customer()
+        {
+
+        }
+
+        public Customer(Guid customerId, string name, string surname, CustomerAddress address)
+        {
+            this.customerId = customerId;
+            this.name = name;
+            this.surname = surname;
+            this.address = address;
+        }
+
+        private Guid customerId;
+        public Guid CustomerId => customerId;
+
+        private string name;
+        public string Name => name;
+
+        private string surname;
+        public string Surname => surname;
+
+        private CustomerAddress address;
+        public CustomerAddress Address => address;
     }
 }
